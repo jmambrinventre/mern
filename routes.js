bffc324ff91466100c02e30fde40aa5b85b99903
jmambrinventre <jmambrinventre@gmail.com>
2023-01-19 08:53:13 -0300

    initial API REST project

---
+++ routes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+    req.getConnection((err, conn) => {
+        if(err) return res.send(err);
+
+        conn.query('SELECT * FROM books', (err, rows) => {
+            if(err) return res.send(err);
+
+            res.json(rows)
+        })
+    })
+});
+
+routes.post('/', (req, res) => {
+    req.getConnection((err, conn) => {
+        if(err) return res.send(err)
+        conn.query('INSERT INTO books SET ?', [req.body], (err, rows) => {
+            if(err) return res.send(err);
+
+            
+            res.send('BOOK ADDED')
+        })
+    })
+});
+
+routes.delete('/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if(err) return res.send(err)
+        conn.query('DELETE FROM books WHERE id = ?', [req.params.id], (err, rows) => {
+            if(err) return res.send(err);
+
+            
+            res.send('BOOK DELETED')
+        })
+    })
+});
+
+routes.put('/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if(err) return res.send(err)
+        conn.query('UPDATE books set ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
+            if(err) return res.send(err);
+
+            
+            res.send('BOOK UPDATED')
+        })
+    })
+});
+
+module.exports = routes;
