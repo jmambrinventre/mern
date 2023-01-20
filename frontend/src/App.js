e87f705e5812dd3479824794b6aa08f9687e3912
jmambrinventre <jmambrinventre@gmail.com>
2023-01-20 15:55:49 -0300

    react two

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import Navbar from './Components/Navbar';
 import BookList from './Components/BookList';
+import Form from './Components/Form';
 
 function App() {
 
+  const [book, setBook] = useState({
+    titulo: '',
+    autor: '',
+    edicion: 0
+  })
+
   const [books, setBooks] = useState([])
+
+  const [listUpdated, setListUpdated] = useState(false)
 
   useEffect(() => {
     const getBooks = () => {
@@ -13,7 +22,8 @@ function App() {
       .then(res => setBooks(res))
     }
     getBooks()
-  }, []);
+    setListUpdated(false)
+  }, [listUpdated]);
 
   return (
     <Fragment>
@@ -22,10 +32,11 @@ function App() {
         <div className='row'>
           <div className='col-7'>
             <h2 style={{ textAlign: 'center'}}>Book List</h2>
-            <BookList books={books}/>
+            <BookList book={book} setBook={setBook} books={books} setListUpdated={setListUpdated}/>
           </div>
           <div className='col-5'>
             <h2 style={{ textAlign: 'center'}}>Book Form</h2>
+            <Form book={book} setBook={setBook}/>
           </div>
         </div>
       </div>
--- frontend/src/Components/BookList.js
+++ frontend/src/Components/BookList.js
@@ -1,6 +1,50 @@
 import React from "react";
 
-const BookList = ( {books} ) => {
+const BookList = ( {book, books, setBook, setListUpdated} ) => {
+
+    const handleDelete = (id) => {
+        const requestInit = {
+            method: 'DELETE'
+        }
+        fetch('http://localhost:9000/api/'+ id, requestInit)
+        .then(res => res.text())
+        .then(res => console.log(res))
+
+        setListUpdated(true)
+
+    }
+
+    let{titulo, autor, edicion} = book
+
+    const handleUpdate = (id) => {
+
+        edicion = parseInt(edicion, 10)
+        //validación de los datos
+        if (titulo === '' || autor === '' || edicion <= 0 ) {
+            alert('Todos los campos son obligatorios')
+            return
+        }
+
+        const requestInit = {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(book)
+        }
+        fetch('http://localhost:9000/api/'+ id, requestInit)
+        .then(res => res.text())
+        .then(res => console.log(res))
+
+        //reiniciando state de libro
+        setBook({
+            titulo: '',
+            autor: '',
+            edicion: 0
+        })
+
+        setListUpdated(true)
+
+    }
+
     return(
         <table className="table">
             <thead>
@@ -14,10 +58,20 @@ const BookList = ( {books} ) => {
             <tbody>
                 {books.map(book => (
                     <tr key={book.id}>
-                        <th>{book.id}</th>
-                        <th>{book.titulo}</th>
-                        <th>{book.autor}</th>
-                        <th>{book.edicion}</th>
+                        <td>{book.id}</td>
+                        <td>{book.titulo}</td>
+                        <td>{book.autor}</td>
+                        <td>{book.edicion}</td>
+                        <td>
+                            <div className="mb-3">
+                                <button onClick={() => { handleDelete(book.id) }} className="btn btn-danger">Delete</button>
+                            </div>
+                        </td>
+                        <td>
+                        <div className="mb-3">
+                                <button onClick={() => { handleUpdate(book.id) }} className="btn btn-dark">Update</button>
+                            </div>
+                        </td>
                     </tr>
                 ))}
             </tbody>
